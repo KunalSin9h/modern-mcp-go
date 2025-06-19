77581f9d6763f2119dcf6055f64e5693c324ed32
Kunal Singh <kunalsin9h@gmail.com>
2025-06-19 18:16:43 +0530

    fix: allow http POST request for stablishing SSE connections

--- server/sse.go
+++ server/sse.go
@@ -331,7 +331,7 @@ func (s *SSEServer) Shutdown(ctx context.Context) error {
 // handleSSE handles incoming SSE connection requests.
 // It sets up appropriate headers and creates a new session for the client.
 func (s *SSEServer) handleSSE(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
+	if r.Method != http.MethodGet && r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
